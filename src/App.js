90fef1a2bb52e13a6a0cb6b67f477402c4af5201
Waqas Zaman <66304653+waqaszaman123@users.noreply.github.com>
2020-06-06 16:53:01 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <br/>
+        waqas is great finance expert
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
