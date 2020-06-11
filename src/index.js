96c6248535d5d6a25f975edd8fef5d78cb40858a
Waqas Zaman <66304653+waqaszaman123@users.noreply.github.com>
2020-06-11 20:14:55 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <div><App/></div>,
+  <div><App name="waqas zaman" /></div>,
   document.getElementById('root')
 );
 
